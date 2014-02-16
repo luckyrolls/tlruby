b146b0e8aabb458a18ea2ed98231d232e3e8f139
mark <markmun99@gmail.com>
2014-02-16 12:21:16 -0800

    In Progress Object version

--- tlruby/blackjackobj.rb
+++ tlruby/blackjackobj.rb
@@ -3,6 +3,7 @@ class Card_bunch # bunch of cards, to be usd by hand and deck
   attr_accessor :cards
   end
 
+
 class Card # duh
   attr_accessor :card_type
   attr_accessor :suit
@@ -17,9 +18,11 @@ end
 class Hand < Card_bunch  # Basic Hand
   attr_accessor :score
 
-  def initialize (score)
-    @score = score
+  def initialize
+    @score = 0
+    @cards = []
   end
+
 
   def calc_score
     ace = false
@@ -34,74 +37,96 @@ class Hand < Card_bunch  # Basic Hand
     end
     }
  self.score = self.score + 10 if ace == true and  self.score < 11
+  end
+  def get_card deck
+    cards.push deck.cards.pop
+  end
 
-    end
 end
 
 class Player_hand < Hand # adds player specific stuff
   attr_accessor :bet
   attr_accessor :balance
   attr_accessor :debt_limit
-  def initialize score, bet, balance, debt_limit
-    @bet = bet
-    @balance = balance
-    @debt_limit = debt_limit
-    super(score)
+  def initialize
+    @bet = 25 # default bet
+    @balance = 5000 # starting cash
+    @debt_limit = -5000 # how much you can go into debt
+    @score = 0
+    @cards = []
   end
 end
 
 class Deck < Card_bunch
 
-   def add_suit( suit, offset)
+  def initialize
+    @cards = Array.new
+  end
+
+   def add_suit suit
      n = 0
      until n == 13
-      cards[n + offset] = Card.new n + 1, suit
+      self.cards.push Card.new n + 1, suit
       n= n + 1
      end
    end
 
-   def init_deck
-     add_suit  'Spades', 0
-     add_suit  'Hearts', 13
-     add_suit  'Diamonds', 26
-     add_suit  'Spades', 39
+   def shuffle_deck
+     add_suit  'Spades'
+     add_suit  'Hearts'
+     add_suit  'Diamonds'
+     add_suit  'Clubs'
      self.cards = cards.sort_by { rand }
    end
+  end
+
+
+
+
+
+
+
+player  = Player_hand.new
+dealer = Hand.new
+deck = Deck.new
+deck.shuffle_deck
+# Lets get started with the first 2 cards #
+player.get_card deck
+dealer.get_card deck
+player.get_card deck
+dealer.get_card deck
+# player.show_hand
+
+
+
+
+
+while true
+  puts "enter your bet or q to quit (default is 50 or previous bet)"
+  new_bet = gets.chomp
+  if new_bet == 'q'
+    puts 'Come back and play again! '
+    break
+  end
+end
+
+
+
+
+
+
 
 =begin
-  def init_deck
-  cards[0] = Card.new 3, "spades"
-
-  end
-=end
-
-
-  end
-
-
-# attr_accessor :hand
-
-#    def deck_init
-
-#     end
-
-
-
 mm =  Card.new 3, "Spades"
 ff = Card.new 7, 'hearts'
 gg = Card.new 12, 'clubs'
-
-pp = Player_hand.new 0, 25, 500, 5000
-deck = Deck.new
-deck.cards = Array.new
-deck.init_deck
-
 pp.cards = Array.new
 pp.cards[0] = mm
 pp.cards[1] = ff
 pp.cards[2] = gg
 pp.calc_score
 puts pp.score
+=end
 
 
 
